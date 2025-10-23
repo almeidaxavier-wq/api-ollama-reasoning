fcbfe08b46fd771c24ef28292ea816ac360b976e
Pedrovinicius123 <pvasx123@gmail.com>
2025-10-23 18:09:05 -0300

    Final Commit of backend (Version 1.0)

--- forms/user.py
+++ forms/user.py
@@ -1,11 +1,12 @@
 from wtforms import StringField, SubmitField, IntegerField
-from wtforms.validators import DataRequired, Optional
+from wtforms.validators import DataRequired, Optional, NumberRange
 from flask_wtf import FlaskForm
 
 class SubmitQueryForm(FlaskForm):
     query = StringField("Query ", validators=[DataRequired()])
     context = StringField("AI context ", validators=[DataRequired()])
     log_dir = StringField("Logging Dir Temp", validators=[Optional()])
+    max_depth = IntegerField("Max Depth", validators=[DataRequired(), NumberRange(10, 20)])
     n_tokens = IntegerField("Number of tokens (max)", validators=[Optional()])
     model_name = StringField("Model name", validators=[Optional()])
     submit = SubmitField('submit')
