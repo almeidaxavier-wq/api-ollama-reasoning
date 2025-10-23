7f5b8cd5071183fa625428854cba0a6027debdd4
Pedrovinicius123 <pvasx123@gmail.com>
2025-10-23 20:01:14 -0300

    Update app.py (1.2.1)

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-app.config['MONGODB_HOST'] = f"mongodb+srv://pvasx123:{os.getenv('DB_PASSWORD')}@clusterollamareasoning.scztjqo.mongodb.net/?appName=ClusterOllamaReasoning&retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=false"
+app.config['MONGODB_HOST'] = f"mongodb+srv://pvasx123:{os.getenv('DB_PASSWORD')}@clusterollamareasoning.scztjqo.mongodb.net/?appName=ClusterOllamaReasoning&retryWrites=true&w=majority&tls=true&tlsInsecure=true"
 
 db.init_app(app)
 
