9a9d132808e2d99ee369a5caa522e2830ee56713
almeidaxavier-wq <almeida.xavier@escolar.ifrn.edu.br>
2025-10-24 16:55:53 -0300

    Refactor app.py for threading and MongoDB URI
    Updated MongoDB connection string to use environment variable. Added threading for handling background tasks and modified route functions accordingly.

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for
 from forms.user import SubmitQueryForm
 from markupsafe import Markup
 from markdown import markdown
 from database import db, upload_file, Upload
 from api.model.reasoning import Reasoning
 from dotenv import load_dotenv
+import threading
 import os
-import shutil
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-app.config['MONGODB_HOST'] = f"mongodb+srv://pvasx123:{os.getenv("DB_PASSWORD")}@clusterollamareasoning.scztjqo.mongodb.net/?retryWrites=true&w=majority&appName=ClusterOllamaReasoning"
+app.config['MONGODB_HOST'] = os.getenv("MONGODB_URI")
 
+# Simple set of threads
+threads = []
 db.init_app(app)
 
 def read_markdown_to_html(log_dir:str):
@@ -26,7 +28,18 @@ def read_markdown_to_html(log_dir:str):
     return Markup(html_code)
 
 @app.route("/")
-def index():
+def home():
+    # Simple thread handling
+    print(threads)
+    for thread in threads:
+        if not thread.is_alive():
+            try:
+                thread.start()
+
+            except:
+                thread.join()
+                threads.remove(thread)
+
     return render_template('index.html')
 
 @app.route("/<log_dir>")
@@ -39,12 +52,12 @@ def read(log_dir:str):
 
 @app.route("/load_log_dir/<log_dir>")
 def load(log_dir:str):
-    for file in os.listdir(os.path.join(log_dir, 'steps')):
-        with open(os.path.join(log_dir, 'steps', file), 'rb') as f:
+    for file in os.listdir(os.path.join('/tmp', log_dir, 'steps')):
+        with open(os.path.join('/tmp', log_dir, 'steps', file), 'rb') as f:
             raw = f.read()
             upload_file(temp_log_dir=log_dir, filename=file, raw_file=raw)
 
-    return redirect(url_for('index'))
+    return redirect(url_for('home'))
 
 
 @app.route("/submit_question", methods=["GET", "POST"])
@@ -58,17 +71,18 @@ def submit_question():
         log_dir_temp = form.log_dir.data or 'default_log'
         n_tokens = form.n_tokens.data if form.n_tokens.data is not None else 100000 # Default value
         model_name = form.model_name.data if form.model_name.data else "deepseek-v3.1:671b-cloud"
+        max_depth = form.max_depth.data
 
         # Generates and stores the raw data on database
-        generate(log_dir_temp, query, context, n_tokens=n_tokens, model_name=model_name)
-
-        return redirect(url_for('load', log_dir=log_dir_temp))
+        threads.append(threading.Thread(target=generate, args=(log_dir_temp, query, context, n_tokens, model_name, max_depth)))
+        return redirect(url_for('home'))
+    
     return render_template('form.html', form=form)
 
-def generate(log_dir:str, query:str, context:str, n_tokens:int, model_name:str):
+def generate(log_dir:str, query:str, context:str, n_tokens:int, model_name:str, max_depth:int):
     thinker = Reasoning(
         max_width=5,
-        max_depth=15,
+        max_depth=max_depth,
         model_name=model_name if model_name else "deepseek-v3.1:671b-cloud",
         n_tokens_default=n_tokens
 
@@ -80,3 +94,4 @@ def generate(log_dir:str, query:str, context:str, n_tokens:int, model_name:str):
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
