d2fb3ed0b5584772c2259824c414f93802f34bbb
Pedrovinicius123 <pvasx123@gmail.com>
2025-10-24 17:15:54 -0300

    Files augmented

--- forms/user.py
+++ forms/user.py
@@ -1,5 +1,5 @@
 from wtforms import StringField, SubmitField, IntegerField
-from wtforms.validators import DataRequired, Optional
+from wtforms.validators import DataRequired, Optional, NumberRange
 from flask_wtf import FlaskForm
 
 class SubmitQueryForm(FlaskForm):
@@ -7,6 +7,7 @@ class SubmitQueryForm(FlaskForm):
     context = StringField("AI context ", validators=[DataRequired()])
     log_dir = StringField("Logging Dir Temp", validators=[Optional()])
     n_tokens = IntegerField("Number of tokens (max)", validators=[Optional()])
+    max_depth = IntegerField("Max Depth", validators=[DataRequired(), NumberRange(2, 20)])
     model_name = StringField("Model name", validators=[Optional()])
     submit = SubmitField('submit')
 
